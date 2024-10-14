6679c497f7f4b2e3a49d7a144e9428c4ace60bdc
Polisseni <vitorpolisseni68@gmail.com>
2024-10-14 18:30:27 -0300

    Exercicios com sucessor e antecessor.

---
+++ exercicios/ex005.py
@@ -0,0 +1,5 @@
+n=int(input('Digite um número: '))
+a=n-1
+s=n+1
+print(f'Analisando o número {n}, podemos concluir que seu antecessor é {a} e seu sucessor é {s}')
+#print(f'Analisando o número {n}, podemos concluir que seu antecessor é {n-1} e seu sucessor é {n+1}')

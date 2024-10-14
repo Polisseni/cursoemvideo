5b7f79aedd15cd6093df30b6e29012cc0c3b5903
Polisseni <vitorpolisseni68@gmail.com>
2024-10-14 18:35:47 -0300

    Exercício com dobra, triplo e raiz quadrada.

---
+++ exercicios/ex006.py
@@ -0,0 +1,10 @@
+n=int(input('Digite um número: '))
+d=n*2
+t=n*3
+r=n**(1/2)
+print(f'Seu dobro vale {d} \nO triplo vale {t} \nA raiz quadrada vale {r}')
+#n=int(input('Digite um número: '))
+#print(f'Seu dobro vale: {n*2}, o triplo vale {n*3} e a raiz quadrade vale: {n**(1/2)}')
+#potência tb pode ser calculada por: pow(n,(1/2))
+
+

24eba935cfa1143c61cccfbbb07819122d85d4b0
Polisseni <vitorpolisseni68@gmail.com>
2024-10-14 18:37:30 -0300

    Média aritmética usando a função float.

---
+++ exercicios/ex007.py
@@ -0,0 +1,5 @@
+n=float(input('Digite a primeira nota do aluno: '))
+s=float(input('Digite a segunda nota do aluno: '))
+m=(n+s)/2
+print(f'A média aritmética das duas notas é igual a: {m}')
+#print(f'A média aritmética entre {n:.1f} e {s:.1f} é igual a {m:.1f}')

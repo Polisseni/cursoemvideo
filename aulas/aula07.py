1a8f64e17fbfbe89aeef6509899523b6044be744
Polisseni <vitorpolisseni68@gmail.com>
2024-10-14 18:09:47 -0300

    Aprendi a usar alguns operadores

---
+++ aulas/aula07.py
@@ -0,0 +1,10 @@
+n1=int(input('Digite um valor:'))
+n2=int(input('Digite outro valor:'))
+s=n1+n2
+m=n1*n2
+d=n1/n2
+di=n1//n2
+e=n1**n2
+print('A soma é {}, o produto é {}, e a divisão é igual a {}'.format(s,m,d),end=' ')
+print('Divisão inteira {} e a potencia {}'.format(di,e))
+#print('A soma é {}, \no produto é {}, \ne a divisão é igual a {}'.format(s,m,d),end=' ') A CONTRA BARRA É PRA QUEBRA DE LINHA

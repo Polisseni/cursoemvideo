d7760adcb2578b31c4cba57a23c99c23f7b3ec2a
Polisseni <vitorpolisseni68@gmail.com>
2024-10-14 18:12:26 -0300

    Ainda na biblioteca, alguns exemplos incluindo emojis

---
+++ aulas/aula08b.py
@@ -0,0 +1,13 @@
+import random
+num=random.random()
+print(num)
+
+import random
+num=random.randint(1,10)
+print(num)
+
+#import emoji
+#print(emoji.emojize("Olá, Mundo!!:globe_showing_americas:", use_aliases=True))
+
+import emoji
+print(emoji.emojize("Olá, Mundo!🌎"))

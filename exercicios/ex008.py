ecd877acaa603a8e47daffeb9bfc8c04b4724427
Polisseni <vitorpolisseni68@gmail.com>
2024-10-15 16:17:49 -0300

    Exercício envolvendo unidades de medida de comprimento.

---
+++ exercicios/ex008.py
@@ -0,0 +1,7 @@
+#        km hm dam m dm cm mm
+
+medida=float(input('Digite uma distância em metros: '))
+dm=medida*10
+cm=medida*100
+mm=medida*1000
+print(f'A medida de {medida}m corresponde a {cm}cm, {mm}mm, {dm}dm')

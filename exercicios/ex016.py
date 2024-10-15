cf799cc730b2ff9d0edabbcd4ffb5d4a0d362087
Polisseni <vitorpolisseni68@gmail.com>
2024-10-15 17:13:28 -0300

    Aprendendo a utilizar bibliotecas.

---
+++ exercicios/ex016.py
@@ -0,0 +1,10 @@
+import math
+num=float(input('Digite um número: '))
+print(f'A porção inteira desse número é: {math.trunc(num)}')
+
+from math import trunc
+num=float(input('Digite um número: '))
+print(f'A porção interira desse número é: {trunc(num)}')
+
+#num=float(input('Digite um número: '))
+#print(f'A porção inteira desse número é: {int(num)}')

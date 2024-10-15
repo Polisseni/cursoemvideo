684a36d5249bbedd4a068b9ce8dcd09510f11b80
Polisseni <vitorpolisseni68@gmail.com>
2024-10-15 17:05:46 -0300

    Programa que calcula a quantidade de tinta necessária por mxm.

---
+++ exercicios/ex011.py
@@ -0,0 +1,6 @@
+alt=float(input('Digite a altura da parede: '))
+larg=float(input('Digite a largura da parede: '))
+área=alt*larg
+print(f'Sua parede tem a dimensão de {alt}x{larg} e a área de {área}m²')
+tinta=área/2
+print(f'Serão necessários {tinta} litros de tinta para pintar essa parede.')

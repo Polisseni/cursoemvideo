4b8576d513bcd5607a581659d71997c3059e2ff6
Polisseni <vitorpolisseni68@gmail.com>
2024-10-15 16:52:30 -0300

    Construindo um programa conversor do real para o dólar.

---
+++ exercicios/ex010.py
@@ -0,0 +1,5 @@
+real=float(input('Quantos reais você tem na carteira?:R$ '))
+dólar=real/5.45
+print(f'Com R${real:.2f} reais você consegue comprar US${dólar:.2f} dólares')
+
+#cotação do dólar em 30/09/2024: R$5,45

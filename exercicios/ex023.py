4fb110817e43b2bac9f7e96a5825632bd88e149f
Polisseni <vitorpolisseni68@gmail.com>
2024-10-31 18:53:42 -0300

    Separando digitos de um número

---
+++ exercicios/ex023.py
@@ -0,0 +1,18 @@
+num = int(input('Digite um número: '))
+#n = str(num)
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print(f'Analisando o número {num}')
+#print(f'Unidade: {n[3]}')
+#print(f'Dezana: {n[2]}')
+#print(f'Centena: {n[1]}')
+#print(f'Milhar: {n[0]}')
+
+
+print(f'Unidade: {u}')
+print(f'Dezana: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')

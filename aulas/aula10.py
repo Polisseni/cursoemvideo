50a63b9225a1843e579fd13b31b3d6e60c4499b3
Polisseni <vitorpolisseni68@gmail.com>
2024-11-06 18:01:09 -0300

    IF e ELSE

---
+++ aulas/aula10.py
@@ -0,0 +1,22 @@
+#tempo = int(input('Seu carro tem quantos anos? '))
+#if tempo <=3:
+#    print('Seu carro é novo')
+#else:
+#    print('Seu carro é velho')
+#print('==FIM==')
+
+
+
+#tempo = int(input('Seu carro tem quantos anos? '))
+#print('Carro Novo'if tempo<=3 else 'Carro velho')
+#print('==FIM==')
+
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite a sua segunda nota: '))
+m = (n1 + n2)/2
+print(f'A sua média foi: {m:.1f}')
+if m>=6.0:
+    print('Sua nota está na média, parabéns!')
+else:
+    print('Sua nota está abaixo da média, ESTUDE MAIS!!')

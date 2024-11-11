373b7ed6a61b9c36aeb1619b06b5cc6e4c0b4325
Polisseni <vitorpolisseni68@gmail.com>
2024-11-11 16:53:29 -0300

    Analisando Um Triângulo 2.0

---
+++ exercicios/ex042.py
@@ -0,0 +1,16 @@
+print('-=-'*20)
+print('ANALISADOR DE TRINÂNGULOS 2.0')
+print('-=-'*20)
+r1 = float(input('Primeiro segmento de reta: '))
+r2 = float(input('Segundo segmento de reta: '))
+r3 = float(input('Terceiro segmento de reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM formar um TRIÂNGULO', end=' ')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO')
+    elif r1 != r2 != r3 != r1: #não esquecer da interação entre r1 e r3
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos acima NÃO PODEM formar um TRIÂNGULO')

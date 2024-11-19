f6b7f44778d34b494d5037c340499f4afb207ea0
Polisseni <vitorpolisseni68@gmail.com>
2024-11-19 15:42:56 -0300

    Progressão Aritimética

---
+++ exercicios/ex051.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Digite o primeiro termo: '))
+razão = int(input('Digite a razão: '))
+décimotermo= primeiro + (10 - 1) * razão
+for c in range (primeiro, décimotermo + razão, razão):
+    print(f'{c}', end='>>>')
+10print('ACABOU')

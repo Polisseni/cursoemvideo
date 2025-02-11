3cee559c9d467f9a8f776e7cd4042126323390c7
Polisseni <vitorpolisseni68@gmail.com>
2025-02-11 16:57:19 -0300

    Um Print Especial

---
+++ exercicios/ex097.py
@@ -0,0 +1,11 @@
+def escreva (msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#PROGRAMA PRINCIPAL
+escreva('Gustavo Guanabara')
+escreva('Aprendendo a Programar em Python')
+escreva('Curso em Vídeo')
